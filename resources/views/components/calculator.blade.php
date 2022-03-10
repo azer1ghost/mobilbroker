5c202ad03c44cd7e5c246a10b1ea7a74a7389816
Gafar <qafarzade2014@gmail.com>
2022-03-10 16:16:07 +0400

    translates

--- resources/views/components/calculator.blade.php
+++ resources/views/components/calculator.blade.php
@@ -1,10 +1,12 @@
-<section class=" home-calculator">
-    <div class="container my-3">
+<section class="home-calculator"  style="margin-top:50px;margin-bottom:50px;">
+    <div class="container my-4">
         <div class="row">
             <div class="col-12">
-                <h1 class="title">@lang('translates.calculator_title')</h1>
+                <h1 class="text-center" style="font-size: 2.6rem; color: #99cd08;">@lang('translates.calculator_title')</h1>
                 <p style="font-family: 'Readex Pro', sans-serif; font-size: 14.4px;"> @lang('translates.calculator_description')</p>
-                <a href="https://customs.gov.az/az/ferdler-ucun/avtomobillerin-getirilmesi/" target="_blank" class="btn-black">@lang('translates.calculator_button')</a>
+                <div class="text-center">
+                    <a href="https://customs.gov.az/az/ferdler-ucun/avtomobillerin-getirilmesi/" target="_blank" class="btn-black">@lang('translates.calculator_button')</a>
+                </div>
             </div>
         </div>
     </div>
