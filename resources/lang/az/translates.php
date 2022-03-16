468a540425141463c00b4e78c9d49f6e1d66bd5f
Gafar <qafarzade2014@gmail.com>
2022-03-16 16:06:20 +0400

    translates

--- resources/lang/az/translates.php
+++ resources/lang/az/translates.php
@@ -25,7 +25,7 @@ return [
     'communication' => 'Əlaqə Vasitələri',
     'menu' => 'Menyular',
     'services' => 'Xidmətlərimiz',
-    'online_contact' => 'Onlayn Müraciət',
+    'online_contact' => 'Onlayn Müraciət Et',
     'rights' => 'Bütün hüquqlar qorunur.',
     'footer_description' => 'Mobil Broker 2017-ci ildən etibarən fəaliyyət göstərən ölkənin ən qabaqcıl gömrük brokeri şirkətidir. Şirkət gömrük orqanları ilə vətəndaşlar arasında etibarlı vasitəçidir. Mobil Broker yerli və xarici vətəndaşların, o cümlədən sahibkarların ölkəyə idxal və ixrac etdikləri yüklərin gömrük rəsmiləşdirilməsində kompleks həllər təklif edir.  Hazırda 5 filial şəbəkəsi ilə fəaliyyət göstərən Mobil Broker Tam Gömrük Təmsilçilik Xidməti ilə müştərilən əlavə zaman və xərclərdən azad olmasını təmin edir.',
 ];
