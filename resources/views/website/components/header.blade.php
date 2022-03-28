0555d2e3252d4b6b81d6bebf825a84ecc1d05486
Gafar <qafarzade2014@gmail.com>
2022-03-28 14:09:23 +0400

    changes

--- resources/views/website/components/header.blade.php
+++ resources/views/website/components/header.blade.php
@@ -1,37 +1,38 @@
 <header>
     <div class="header-top" style=" background-color: white; color:black;font-size: 18px">
         <div class="container d-flex justify-content-between align-items-center py-2" style="">
-            <div class="header-top-left d-flex ">
-                <span class="address" style="color: black"><i class="far fa-map-marker-alt"></i> {{setting('site.address')}}</span>
-            </div>
-            <a class="navbar-brand" href="{{route('homepage')}}">
+{{--            <div class="header-top-left d-flex ">--}}
+{{--                <span class="address" style="color: black"><i class="far fa-map-marker-alt"></i> {{setting('site.address')}}</span>--}}
+{{--            </div>--}}
+            <a class="navbar-brand" style="margin-left: 100px" href="{{route('homepage')}}">
 
             <img src="{{asset( Voyager::image(setting('site.logo')) ?? '/assets/images/logo-white.png') }}" width="150" alt="{{config('app.name')}}" >
             </a>
             <div class="header-top-right d-flex">
-
-                <div>
-                    <a  href="https://api.whatsapp.com/send?phone={{setting('site.phone')}}" target="_blank"><span style="font-size: 23px;color:#99cd08" class="fab fa-whatsapp me-2"></span></a>
-                </div>
-                <div class="mx-2">
-                    <a class="footer-link" style="color: black" href="tel:{{setting('site.short_phone')}}">{{setting('site.short_phone')}}</a>
-                </div>
-
-                <div class="dropdown language-dropdown mx-2">
+                <div class="dropdown mx-2">
                     <div class="dropdown-toggle" id="language" data-bs-toggle="dropdown" aria-expanded="false">
-                        <span class="flag-icon flag-icon-{{app()->getLocale()}}"></span> {{ucfirst(app()->getLocale())}}
+                        {{--                        <span class="flag-icon flag-icon-{{app()->getLocale()}}"></span>--}}
+                        {{ucfirst(app()->getLocale())}}
                     </div>
                     <div class="dropdown-menu" aria-labelledby="language" style="min-width: auto !important;">
                         @foreach(config('voyager.multilingual.locales') as $locale)
                             <li >
                                 <a class="dropdown-item"  href="{{route('locale', $locale)}}">
-                                    <span class="flag-icon flag-icon-{{$locale}}"></span>
+                                    {{--                                    <span class="flag-icon flag-icon-{{$locale}}"></span>--}}
                                     {{ucfirst($locale)}}
                                 </a>
                             </li>
                         @endforeach
                     </div>
                 </div>
+                <div>
+                    <a href="https://api.whatsapp.com/send?phone={{setting('site.phone')}}" target="_blank"><span style="font-size: 23px;color:#99cd08" class="fab fa-whatsapp me-2"></span></a>
+                </div>
+                <div class="mx-2">
+                    <a class="footer-link" style="color: black" href="tel:{{setting('site.short_phone')}}">{{setting('site.short_phone')}}</a>
+                </div>
+
+
             </div>
         </div>
     </div>
