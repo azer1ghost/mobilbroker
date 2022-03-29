c8af419277c83b19158baee138a32deee53e3c7c
Gafar <qafarzade2014@gmail.com>
2022-03-29 11:30:39 +0400

    changes

--- resources/views/website/components/header.blade.php
+++ resources/views/website/components/header.blade.php
@@ -7,8 +7,8 @@
             <a class="navbar-brand" style="margin-left: 115px" href="{{route('homepage')}}">
                 <img src="{{asset( Voyager::image(setting('site.logo')) ?? '/assets/images/logo-white.png') }}" width="150" alt="{{config('app.name')}}" >
             </a>
-            <div class="header-top-right d-flex" style="margin-right: 115px;color: #111f6e">
-                <div class="dropdown" style="margin-right: 40px">
+            <div class="header-top-right d-flex" style="margin-right: 110px;color: #111f6e">
+                <div class="dropdown" style="margin-right: 66px">
                     <div class="dropdown-toggle"  id="language" data-bs-toggle="dropdown" aria-expanded="false">
                         {{--                        <span class="flag-icon flag-icon-{{app()->getLocale()}}"></span>--}}
                         {{ucfirst(app()->getLocale())}}
