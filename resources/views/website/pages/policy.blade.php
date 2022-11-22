852d49ac8afdc535b6d3e45b64d61a1c7f638924
Valiyev <aliveliyev016@gmail.com>
2022-11-22 11:33:36 +0400

    policy

--- resources/views/website/pages/policy.blade.php
+++ resources/views/website/pages/policy.blade.php
@@ -4,9 +4,18 @@
 @section('description', 'Policy')
 
 @section('content')
-            <div class="row my-2">
-                <div class="col-12" style="text-align: center">
-                    <img style="border-radius: 15px" src="{{Voyager::image(setting('site.policy'))}}"  alt="About US image">
-                </div>
-            </div>
+    <div class="row my-2">
+        <div class="col-12" style="text-align: center">
+
+             <img style="border-radius: 15px"  alt="Policy image" src=
+                  @if(app()->getLocale() == 'az')
+                  "{{Voyager::image(setting('site.policy'))}}"
+                  @elseif(app()->getLocale() == 'ru')
+                    "{{Voyager::image(setting('site.policy_ru'))}}"
+                  @else
+                  "{{Voyager::image(setting('site.policy_en'))}}"
+                  @endif
+                 >
+        </div>
+    </div>
 @endsection
