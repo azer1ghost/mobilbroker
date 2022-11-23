d39e86c7ad35897c6cc7a3f79ed8ccd78c2faec3
Valiyev <aliveliyev016@gmail.com>
2022-11-23 12:25:47 +0400

    brands

--- resources/views/components/brands.blade.php
+++ resources/views/components/brands.blade.php
@@ -2,9 +2,9 @@
     <div class="col-12">
         <h1 class="text-center" style="font-size: 2.6rem; color: #99cd08;">@lang('translates.partners')</h1>
     </div>
-    <div class="owl-carousel">
+    <div class="owl-carousel" style="padding-top: 1rem !important;padding-bottom: 1rem !important;">
         @foreach($brands as $brand)
-            <div class="item" style="padding-top: 1rem !important;padding-bottom: 1rem !important;">
+            <div class="item" >
                 <img src="{{asset(Voyager::image($brand->getAttribute('image')))}}"  height="100" alt="{{$brand->getAttribute('title')}}">
             </div>
         @endforeach
